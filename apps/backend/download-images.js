ae78b2a5f2e414c1ed6c4dcab277bb6cfeb352cb
Andrew Cannon <drewcannon3@MacBook-Air-5.local>
2025-09-30 14:16:16 -0400

    migrations bug fix

--- apps/backend/download-images.js
+++ apps/backend/download-images.js
@@ -55,7 +55,8 @@ async function downloadImage(url, filepath) {
     method: 'GET',
     responseType: 'stream',
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+      'Referer': 'https://www.tcdb.com/'
     }
   });
 
